22f5c7c5e1742dc62c1edb6eee666f3167124ec5
ktsiounis <konstantinos.tsiounis@hotmail.com>
2017-02-20 20:17:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/konstantinos/mobilecontroller/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.konstantinos.mobilecontroller;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView devicelist;
+    Button paired;
+    private BluetoothAdapter mBluetooth;
+    private Set pairedDevices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        devicelist = (ListView)findViewById(R.id.deviceList);
+
+        mBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetooth == null){
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available!", Toast.LENGTH_LONG);
+            //finish th app
+            finish();
+        }
+        else {
+            if(mBluetooth.isEnabled()){
+
+            }
+            else{
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon,1);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void connectWithDevice(MenuItem item){
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
